6c10ed1b6999ef0119485aa869f7c755f4049d07
Elias Müller <elias.mr1@gmail.com>
2022-12-01 20:21:11 +0100

    Add day 1 part 1

---
+++ src/advent_of_code1.rs
@@ -0,0 +1,70 @@
+mod files {
+    use std::fs::File;
+    use std::io;
+    use std::io::{BufRead, BufReader, Lines};
+    use std::path::Path;
+
+    pub fn parse_lines_from<P>(file: P) -> Vec<String> where P: AsRef<Path> {
+        let mut lines: Vec<String> = Vec::new();
+        if let Ok(file_lines) = read_lines(file) {
+            for line in file_lines {
+                if let Ok(ip) = line {
+                    lines.push(ip);
+                }
+            }
+        }
+        return lines;
+    }
+
+    fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
+        where P: AsRef<Path>, {
+        let file = File::open(filename)?;
+        Ok(BufReader::new(file).lines())
+    }
+}
+
+mod problem {
+
+    pub struct Elve {
+        calories: Vec<u32>,
+    }
+
+    impl Elve {
+        pub fn new() -> Self {
+            Elve { calories: Vec::new() }
+        }
+
+        pub fn calories(&self) -> u32 {
+            return self.calories.iter().sum();
+        }
+    }
+
+    pub fn parse_elves_from(lines: Vec<String>) -> Vec<Elve> {
+        let mut elves = Vec::new();
+        let mut current_elve = Elve::new();
+        for line in lines {
+            if line.is_empty() {
+                elves.push(current_elve);
+                current_elve = Elve::new();
+            } else {
+                let food = line.parse::<u32>().unwrap();
+                current_elve.calories.push(food)
+            }
+        }
+        return elves;
+    }
+}
+
+
+
+pub fn solve() {
+    let file = "resources/day_1_puzzle_1_input.txt";
+
+    let lines = files::parse_lines_from(file);
+    let elves = problem::parse_elves_from(lines);
+
+    let calories = elves.iter().map(|x| x.calories()).max().unwrap();
+    println!("Elve with most food {}", calories)
+
+}
+
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod advent_of_code1;
+
+fn main() {
+    println!("Advent of Code: 1");
+    advent_of_code1::solve()
+}
