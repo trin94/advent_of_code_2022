31d05f632408af97a349be915cfdb16b525bf019
Elias Müller <elias.mr1@gmail.com>
2022-12-08 20:19:19 +0100

    Add day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -54,7 +54,7 @@ impl Grid {
 
     fn can_view_upper_border(&self, x: usize, y: usize) -> bool {
         let item = self.item(x, y);
-        self.elements_atop_of(x, y)
+        self.elements_above_of(x, y)
             .iter()
             .all(|e| e < &item)
     }
@@ -82,7 +82,7 @@ impl Grid {
             .collect::<Vec<&usize>>()
     }
 
-    fn elements_atop_of(&self, x: usize, y: usize) -> Vec<&usize> {
+    fn elements_above_of(&self, x: usize, y: usize) -> Vec<&usize> {
         let start = x;
         let end = y * self.size + x;
         self.elements[start..end]
@@ -100,21 +100,84 @@ impl Grid {
             .step_by(self.size)
             .collect::<Vec<&usize>>()
     }
+
+    pub fn scenic_score_of(&self, x: usize, y: usize) -> usize {
+        let left = self.viewing_distance_left_of(x, y);
+        let right = self.viewing_distance_right_of(x, y);
+        let above = self.viewing_distance_above_of(x, y);
+        let below = self.viewing_distance_below_of(x, y);
+        left * right * above * below
+    }
+
+    fn viewing_distance_left_of(&self, x: usize, y: usize) -> usize {
+        let item = self.item(x, y);
+        let elements = self.elements_left_of(x, y);
+        let smaller = elements
+            .iter()
+            .rev()
+            .take_while(|e| e < &&item)
+            .count();
+        cmp::min(smaller + 1, elements.len())
+    }
+
+    fn viewing_distance_right_of(&self, x: usize, y: usize) -> usize {
+        let item = self.item(x, y);
+        let elements = self.elements_right_of(x, y);
+        let smaller = elements
+            .iter()
+            .take_while(|e| e < &&item)
+            .count();
+        cmp::min(smaller + 1, elements.len())
+    }
+
+    fn viewing_distance_above_of(&self, x: usize, y: usize) -> usize {
+        let item = self.item(x, y);
+        let elements = self.elements_above_of(x, y);
+        let smaller = elements
+            .iter()
+            .rev()
+            .take_while(|e| e < &&item)
+            .count();
+        cmp::min(smaller + 1, elements.len())
+    }
+
+    fn viewing_distance_below_of(&self, x: usize, y: usize) -> usize {
+        let item = self.item(x, y);
+        let elements = self.elements_below_of(x, y);
+        let smaller = elements
+            .iter()
+            .take_while(|e| e < &&item)
+            .count();
+        cmp::min(smaller + 1, elements.len())
+    }
 }
 
 
 pub fn solve() {
     let file = "resources/day08.txt";
     let lines = files::parse_string_from(file).unwrap();
-
-    let mut sum = 0;
     let grid = Grid::new(lines);
 
+    let mut sum = 0;
     for x in 0..(grid.rows()) {
         for y in 0..(grid.column()) {
-            if grid.can_view_border(x, y) { sum += 1; }
+            if grid.can_view_border(x, y) {
+                sum += 1;
+            }
         }
     }
 
-    println!("Visible from outside the grid: {}", sum)
+    println!("Visible outside: {}", sum);
+
+    let mut scenic = 0;
+    for x in 0..(grid.rows()) {
+        for y in 0..(grid.column()) {
+            let new_scenic = grid.scenic_score_of(x, y);
+            if new_scenic > scenic {
+                scenic = new_scenic;
+            }
+        }
+    }
+
+    println!("Scenic score: {}", scenic);
 }
